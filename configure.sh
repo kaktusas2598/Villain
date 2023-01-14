334dcc39f92f83f5ec31e3e8489b396f2ecee65a
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2023-01-14 15:41:55 +0000

    Improve config script

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git submodule add https://github.com/g-truc/glm.git vendor/glm
+#git submodule add https://github.com/g-truc/glm.git vendor/glm
+git submodule update --init --recursive
 
 rm -rf build/*
 # Can specify option with cmake -DOPTION_NAME=VALUE
