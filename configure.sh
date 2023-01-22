844a28236a21b29bbaa98819b41f90fe9a23ecc9
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2023-01-22 00:02:58 +0000

    cmake stuff

--- configure.sh
+++ configure.sh
@@ -5,4 +5,7 @@ git submodule update --init --recursive
 
 rm -rf build/*
 # Can specify option with cmake -DOPTION_NAME=VALUE
+#cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo
+#cmake -DCMAKE_BUILD_TYPE=Debug
 cmake -S . -B build/ -G"Unix Makefiles"
+#-G"Visual Studio 15 2017"
