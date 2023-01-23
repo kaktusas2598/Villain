722a6287170e278c553552c3d7498b6cd90aa389
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2023-01-23 23:23:11 +0000

    Engine can now be used as a library!

---
+++ examples/2D/assets/scripts/config.lua
@@ -0,0 +1,11 @@
+rendering = {
+	SDLRenderingEnabled = true -- Optional SDL Rendering
+}
+window = {
+	title = "Vigilant Engine Test",
+	width = 1024, --800,
+	height = 768, --600,
+	fullscreen = false,
+	borderless = false,
+	resizable = true
+}
