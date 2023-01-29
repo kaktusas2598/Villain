a91170b249089d53c627ee0ee385ac52c0b433cc
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2023-01-29 22:51:41 +0000

    Minor things

--- src/Vertex.hpp
+++ src/Vertex.hpp
@@ -7,6 +7,6 @@ struct Vertex {
     glm::vec3 Position;
     glm::vec3 Normal;
     //glm::vec3 Color;
-    glm::vec2 TexCoords;
+    glm::vec2 TexCoords; //TODO: Rename to UV
 };
 #endif // __Vertex__
