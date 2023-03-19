91e6ec9dd162c0a48451564e972793f1e323841b
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-03-19 15:55:59 +0000

    Engine: Move Texture class to Villain namespace

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -4,73 +4,76 @@
 
 #include <iostream>
 
-Texture::Texture(const std::string& fileName)
-    : rendererID(0), filePath(fileName), localBuffer(nullptr), width(0), height(0), BPP(0), target(GL_TEXTURE_2D) {
+namespace Villain {
 
-    stbi_set_flip_vertically_on_load(1);
+    Texture::Texture(const std::string& fileName)
+        : rendererID(0), filePath(fileName), localBuffer(nullptr), width(0), height(0), BPP(0), target(GL_TEXTURE_2D) {
 
-    localBuffer = stbi_load(fileName.c_str(), &width, &height, &BPP, 4);
+        stbi_set_flip_vertically_on_load(1);
 
-    GLCall(glGenTextures(1, &rendererID));
-    GLCall(glBindTexture(GL_TEXTURE_2D, rendererID));
+        localBuffer = stbi_load(fileName.c_str(), &width, &height, &BPP, 4);
 
-    // Tell open GL how to filter texture when minifying or magnifying
-    // how to wrap texture on x(s) and y(t) axis
-    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
-    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
-    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
-    // TODO: do I need mipmap stuff?
+        GLCall(glGenTextures(1, &rendererID));
+        GLCall(glBindTexture(GL_TEXTURE_2D, rendererID));
 
-    // Generate texture
-    GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, localBuffer));
-    GLCall(glBindTexture(GL_TEXTURE_2D, 0));
+        // Tell open GL how to filter texture when minifying or magnifying
+        // how to wrap texture on x(s) and y(t) axis
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+        // TODO: do I need mipmap stuff?
 
-    if (localBuffer)
-        stbi_image_free(localBuffer);
-}
+        // Generate texture
+        GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, localBuffer));
+        GLCall(glBindTexture(GL_TEXTURE_2D, 0));
 
-Texture::Texture(std::vector<std::string> faces)
-    : rendererID(0), localBuffer(nullptr), width(0), height(0), BPP(0), target(GL_TEXTURE_CUBE_MAP) {
-    stbi_set_flip_vertically_on_load(0);
-
-    GLCall(glGenTextures(1, &rendererID));
-    GLCall(glBindTexture(GL_TEXTURE_CUBE_MAP, rendererID));
-
-    for (unsigned int i = 0; i < faces.size(); i++) {
-        localBuffer = stbi_load(faces[i].c_str(), &width, &height, &BPP, 0);
-
-        if (localBuffer) {
-            GLCall(glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0,
-                        GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, localBuffer));
+        if (localBuffer)
             stbi_image_free(localBuffer);
-        } else {
-            std::cout << "Failed loading cubemap texture : " << faces[i] << std::endl;
-        }
     }
 
-    GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
-    GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
-    GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-    GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
-    GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE));
+    Texture::Texture(std::vector<std::string> faces)
+        : rendererID(0), localBuffer(nullptr), width(0), height(0), BPP(0), target(GL_TEXTURE_CUBE_MAP) {
+        stbi_set_flip_vertically_on_load(0);
 
-    GLCall(glBindTexture(GL_TEXTURE_2D, 0));
+        GLCall(glGenTextures(1, &rendererID));
+        GLCall(glBindTexture(GL_TEXTURE_CUBE_MAP, rendererID));
 
-    stbi_set_flip_vertically_on_load(1);
+        for (unsigned int i = 0; i < faces.size(); i++) {
+            localBuffer = stbi_load(faces[i].c_str(), &width, &height, &BPP, 0);
+
+            if (localBuffer) {
+                GLCall(glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0,
+                            GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, localBuffer));
+                stbi_image_free(localBuffer);
+            } else {
+                std::cout << "Failed loading cubemap texture : " << faces[i] << std::endl;
+            }
+        }
+
+        GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+        GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+        GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+        GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+        GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE));
+
+        GLCall(glBindTexture(GL_TEXTURE_2D, 0));
+
+        stbi_set_flip_vertically_on_load(1);
+    }
+
+    Texture::~Texture() {
+        std::cout << "Deleting texture: " << filePath << std::endl;
+        GLCall(glDeleteTextures(1, &rendererID));
+    }
+
+    void Texture::bind(unsigned int slot) const {
+        GLCall(glActiveTexture(GL_TEXTURE0 + slot));
+        GLCall(glBindTexture(target, rendererID));
+    }
+
+    void Texture::unbind() const {
+        GLCall(glBindTexture(target, 0));
+    }
+
 }
-
-Texture::~Texture() {
-    std::cout << "Deleting texture: " << filePath << std::endl;
-    GLCall(glDeleteTextures(1, &rendererID));
-}
-
-void Texture::bind(unsigned int slot) const {
-    GLCall(glActiveTexture(GL_TEXTURE0 + slot));
-    GLCall(glBindTexture(target, rendererID));
-}
-
-void Texture::unbind() const {
-    GLCall(glBindTexture(target, 0));
-}
-
--- src/Texture.hpp
+++ src/Texture.hpp
@@ -4,31 +4,34 @@
 #include "ErrorHandler.hpp"
 #include <vector>
 
-class Texture {
-    public:
-        // 2D Texture
-        Texture(const std::string& fileName);
-        // Cubemap Texture
-        Texture(std::vector<std::string> faces);
-        ~Texture();
+namespace Villain  {
 
-        void bind(unsigned int slot = 0) const;
-        void unbind() const;
+    class Texture {
+        public:
+            // 2D Texture
+            Texture(const std::string& fileName);
+            // Cubemap Texture
+            Texture(std::vector<std::string> faces);
+            ~Texture();
 
-        inline int getWidth() const { return width; }
-        inline int getHeight() const { return height; }
+            void bind(unsigned int slot = 0) const;
+            void unbind() const;
 
-        void setType(std::string& t) { type = t; }
-        inline std::string getType() const { return type; }
+            inline int getWidth() const { return width; }
+            inline int getHeight() const { return height; }
 
-        unsigned int getID() { return rendererID; }
-    private:
-        unsigned int rendererID;
-        std::string filePath;
-        unsigned char* localBuffer;
-        int width, height, BPP; // Bits per picture
-        std::string type;
-        GLenum target; ///< GL_TEXTURE_2D, GL_TEXTURE_CUBE_MAP
-};
+            void setType(std::string& t) { type = t; }
+            inline std::string getType() const { return type; }
+
+            unsigned int getID() { return rendererID; }
+        private:
+            unsigned int rendererID;
+            std::string filePath;
+            unsigned char* localBuffer;
+            int width, height, BPP; // Bits per picture
+            std::string type;
+            GLenum target; ///< GL_TEXTURE_2D, GL_TEXTURE_CUBE_MAP
+    };
+}
 
 #endif // __Texture__
