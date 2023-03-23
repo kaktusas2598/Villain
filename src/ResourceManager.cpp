244f9808ebe84a9722e5ed6447252a44949ee1e1
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-03-23 10:12:37 +0000

    Engine: implementing some helper functions in ResourceManager

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -3,4 +3,22 @@
 namespace Villain {
 
     ResourceManager* ResourceManager::sInstance = nullptr;
+
+    void ResourceManager::clearTextureMap() {
+        textureMap.clear();
+    }
+
+    void ResourceManager::clearTexture(std::string id) {
+        delete textureMap[id];
+        textureMap.erase(id);
+    }
+
+    void ResourceManager::clearShaderMap() {
+        shaderMap.clear();
+    }
+
+    void ResourceManager::clearShader(std::string id) {
+        delete shaderMap[id];
+        shaderMap.erase(id);
+    }
 }
