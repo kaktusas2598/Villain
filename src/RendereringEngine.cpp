bbba96b45a00a7704bb43b722efb29ce669590fe
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-04-04 17:02:09 +0100

    Engine: forward rendering for multiple lights is here! We desperately need frustum culling now!

--- src/RendereringEngine.cpp
+++ src/RendereringEngine.cpp
@@ -16,16 +16,50 @@ namespace Villain {
         defaultShader->createFromResource("forward-ambient");
 
         mainCamera = new Camera3D();
+
+        glFrontFace(GL_CW);
+        glCullFace(GL_BACK);
+        glEnable(GL_CULL_FACE);
+        glEnable(GL_DEPTH_TEST);
+    }
+
+    RenderingEngine::~RenderingEngine() {
+        delete defaultShader;
     }
 
     void RenderingEngine::render(SceneNode* node) {
+        // TODO: this can be set in imgui, need getter/setter in RenderingEngine
+        defaultShader->bind();
+        defaultShader->setUniformVec3("color", glm::vec3(0.8f, 0.6f, 0.2f));
         node->render(defaultShader, this, mainCamera);
-
 
         // Loop through lights now
         // Render all lights, generate shadow maps, etc
         for (auto& light: lights) {
+            activeLight = light;
 
+            // HACK: for now all spot lights will act as a flashlight
+            // and use main camera's position
+            if (activeLight->type() == "spot") {
+                SpotLight* flashlight = dynamic_cast<SpotLight*>(activeLight);
+                flashlight->Position = mainCamera->getPosition();
+                flashlight->Direction = dynamic_cast<Camera3D*>(mainCamera)->getFront();
+            }
+
+            //// NOTE: shadow mapping techniques will be here
+
+            //// Using additive blending here to render lights one by one and blend onto the scene
+            //// this is so called forward multi-pass rendering
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_ONE, GL_ONE);
+            glDepthMask(GL_FALSE);
+            glDepthFunc(GL_EQUAL);
+
+            node->render(light->getShader(), this, mainCamera);
+
+            glDepthMask(GL_TRUE);
+            glDepthFunc(GL_LESS);
+            glDisable(GL_BLEND);
         }
 
     }
--- src/RendereringEngine.hpp
+++ src/RendereringEngine.hpp
@@ -7,25 +7,25 @@
 
 namespace Villain {
 
-    // TODO: move all rendering code to here, at least for scene graphs
     class RenderingEngine {
         public:
             RenderingEngine();
-            ~RenderingEngine() {}
+            ~RenderingEngine();
             void render(SceneNode* node);
 
-            inline void addLight(BaseLight& light) { lights.push_back(&light); }
+            inline void addLight(BaseLight* light) { lights.push_back(light); }
+            inline BaseLight* getActiveLight() { return activeLight; }
             void setMainCamera(Camera& camera) { mainCamera = &camera; }
+            inline Camera* getMainCamera() { return mainCamera; }
             // Call on window/viewport resize event
             void resizeCameras(int newWidth, int newHeight) { mainCamera->rescale(newWidth, newHeight); }
         private:
             // TODO: will need to support multiple cameras at once
             Camera* mainCamera;
 
-            // NOTE: Will need to add way more shaders or do sth different
-            // forward shaders for each light type: ambient, directional, point and spot
-            Shader* defaultShader;
+            Shader* defaultShader = nullptr;
 
+            BaseLight* activeLight = nullptr;
             std::vector<BaseLight*> lights;
     };
 }
