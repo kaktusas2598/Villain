e5d044ef9a8d85297ef780ba3213fd971f1c37fd
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-04-06 23:48:57 +0100

    Engine: refactor Plane struct into separate class in physics module

---
+++ src/physics/Plane.cpp
@@ -0,0 +1,19 @@
+#include "Plane.hpp"
+
+namespace Villain {
+
+    Plane Plane::normalized() const {
+        float magnitude = normal.length();
+
+        return Plane(normal/magnitude, distance/magnitude);
+    }
+
+    IntersectData Plane::intersectSphere(const BoundingSphere& sphere) {
+        // NOTE: does this do same thing as getSignedDistanceToPlane(glm::vec3&)?
+        // super similar
+        float distanceFromSphereCentre = fabs(glm::dot(normal, sphere.getCentre()) + distance);
+        float distanceFromSphere = distanceFromSphereCentre - sphere.getRadius();
+
+        return IntersectData(distanceFromSphere < 0.f, distanceFromSphere);
+    }
+}
