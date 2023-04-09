cba8138f87410febfffb19a2dacd77fa01807967
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-04-09 22:48:25 +0100

    FPS: map update

--- examples/FPS/src/Game.cpp
+++ examples/FPS/src/Game.cpp
@@ -57,7 +57,7 @@ void Game::init() {
     camera = new Camera3D();
     camera->setZPlanes(0.1f, 1000.f); // for bigger render range
 
-    SceneNode* player = (new SceneNode("Player", glm::vec3(2.f, 1.f, 17.5f)))->addComponent(new CameraComponent(camera));
+    SceneNode* player = (new SceneNode("Player", glm::vec3(3.f, 1.f, 17.5f)))->addComponent(new CameraComponent(camera));
     player->addComponent(new Player(currentLevel));
     player->addComponent(new LookController());
     //player->addComponent(new MoveController());
