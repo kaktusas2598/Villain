b41a8bc00e79bc8d902a05bc44d999d2e724140e
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-04-09 01:19:06 +0100

    FPS: small Level refactor

--- examples/FPS/src/Level.cpp
+++ examples/FPS/src/Level.cpp
@@ -21,66 +21,41 @@ void Level::generateLevel(const std::string& tileAtlasFileName) {
     for (int i = 0; i < bitmap->getWidth(); i++) {
         for (int j = 0; j < bitmap->getHeight(); j++) {
             Pixel pixel = bitmap->getPixel(i, j);
-
-            // Selects tile from atlas using green component of the pixel in current position in bitmap
-            float* texCoords = getTexCoords(pixel.G);
-            float xHigher = texCoords[0], xLower = texCoords[1];
-            float yHigher = texCoords[2], yLower = texCoords[3];
-
             if (pixel.R != 0 || pixel.G != 0 || pixel.B != 0) {
+                float* texCoords = getTexCoords(pixel.G);
                 // Floor vertices, normals are defaults just to make shader work
                 addFace(&indices, vertices.size(), true);
-                vertices.push_back({glm::vec3((i * ROOM_WIDTH), 0.0f, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yLower)});
-                vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), 0.0f, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yLower)});
-                vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), 0.0f, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yHigher)});
-                vertices.push_back({glm::vec3((i * ROOM_WIDTH), 0.0f, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yHigher)});
+                addVertices(&vertices, i, j, true, false, true, 0.0f, texCoords);
 
                 // Ceilings, also revert indices to make sure textures are drawn from the bottom
                 addFace(&indices, vertices.size(), false);
-                vertices.push_back({glm::vec3((i * ROOM_WIDTH), ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yLower)});
-                vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yLower)});
-                vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yHigher)});
-                vertices.push_back({glm::vec3((i * ROOM_WIDTH), ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yHigher)});
+                addVertices(&vertices, i, j, true, false, true, 1.0f, texCoords);
 
                 texCoords = getTexCoords(pixel.R);
-                xHigher = texCoords[0], xLower = texCoords[1];
-                yHigher = texCoords[2], yLower = texCoords[3];
                 // Genertate walls
                 Pixel pixel = bitmap->getPixel(i, j - 1); // get adjacent pixel
                 if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0) {
                     // Generate left wall
                     addFace(&indices, vertices.size(), false);
-                    vertices.push_back({glm::vec3((i * ROOM_WIDTH), 0.0f, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yLower)});
-                    vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), 0.0f, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yLower)});
-                    vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yHigher)});
-                    vertices.push_back({glm::vec3((i * ROOM_WIDTH), ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yHigher)});
+                    addVertices(&vertices, i, 0, true, true, false, j, texCoords);
                 }
                 pixel = bitmap->getPixel(i, j + 1);
                 if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0) {
                     // Generate right wall
                     addFace(&indices, vertices.size(), true);
-                    vertices.push_back({glm::vec3((i * ROOM_WIDTH), 0.0f, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yLower)});
-                    vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), 0.0f, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yLower)});
-                    vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yHigher)});
-                    vertices.push_back({glm::vec3((i * ROOM_WIDTH), ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yHigher)});
+                    addVertices(&vertices, i, 0, true, true, false, (j + 1), texCoords);
                 }
                 pixel = bitmap->getPixel(i - 1, j);
                 if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0) {
                     // Generate near wall
                     addFace(&indices, vertices.size(), true);
-                    vertices.push_back({glm::vec3((i * ROOM_WIDTH), 0.0f, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yLower)});
-                    vertices.push_back({glm::vec3((i * ROOM_WIDTH), 0.0f, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yLower)});
-                    vertices.push_back({glm::vec3((i * ROOM_WIDTH), ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yHigher)});
-                    vertices.push_back({glm::vec3((i * ROOM_WIDTH), ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yHigher)});
+                    addVertices(&vertices, 0, j, false, true, true, i, texCoords);
                 }
                 pixel = bitmap->getPixel(i + 1, j);
                 if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0) {
                     // Generate far wall
                     addFace(&indices, vertices.size(), false);
-                    vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), 0.0f, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yLower)});
-                    vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), 0.0f, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yLower)});
-                    vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xHigher, yHigher)});
-                    vertices.push_back({glm::vec3(((i + 1) * ROOM_WIDTH), ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(xLower, yHigher)});
+                    addVertices(&vertices, 0, j, false, true, true, (i + 1), texCoords);
                 }
             }
         }
@@ -106,6 +81,25 @@ void Level::addFace(std::vector<unsigned int>* indices, int startLocation, bool 
         indices->push_back(startLocation + 0);
         indices->push_back(startLocation + 2);
         indices->push_back(startLocation + 3);
+    }
+}
+
+void Level::addVertices(std::vector<VertexP1N1UV>* vertices, int i, int j, bool x, bool y, bool z, float offset, float* texCoords) {
+    if (x && z) { // floor on xz plane
+        vertices->push_back({glm::vec3((i * ROOM_WIDTH), offset * ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(texCoords[1], texCoords[3])});
+        vertices->push_back({glm::vec3(((i + 1) * ROOM_WIDTH), offset * ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(texCoords[0], texCoords[3])});
+        vertices->push_back({glm::vec3(((i + 1) * ROOM_WIDTH), offset * ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(texCoords[0], texCoords[2])});
+        vertices->push_back({glm::vec3((i * ROOM_WIDTH), offset * ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(texCoords[1], texCoords[2])});
+    } else if (x && y) {
+        vertices->push_back({glm::vec3((i * ROOM_WIDTH), (j * ROOM_HEIGHT), offset * ROOM_LENGTH), glm::vec3(0.0f), glm::vec2(texCoords[1], texCoords[3])});
+        vertices->push_back({glm::vec3(((i + 1) * ROOM_WIDTH), (j * ROOM_HEIGHT), offset * ROOM_LENGTH), glm::vec3(0.0f), glm::vec2(texCoords[0], texCoords[3])});
+        vertices->push_back({glm::vec3(((i + 1) * ROOM_WIDTH), (j + 1) * ROOM_HEIGHT, offset * ROOM_LENGTH), glm::vec3(0.0f), glm::vec2(texCoords[0], texCoords[2])});
+        vertices->push_back({glm::vec3((i * ROOM_WIDTH), (j + 1) * ROOM_HEIGHT, offset * ROOM_LENGTH), glm::vec3(0.0f), glm::vec2(texCoords[1], texCoords[2])});
+    } else if (y && z) {
+        vertices->push_back({glm::vec3(offset * ROOM_WIDTH, i * ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(texCoords[1], texCoords[3])});
+        vertices->push_back({glm::vec3(offset * ROOM_WIDTH, i * ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(texCoords[0], texCoords[3])});
+        vertices->push_back({glm::vec3(offset * ROOM_WIDTH, (i + 1) * ROOM_HEIGHT, ((j + 1) * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(texCoords[0], texCoords[2])});
+        vertices->push_back({glm::vec3(offset * ROOM_WIDTH, (i + 1) * ROOM_HEIGHT, (j * ROOM_LENGTH)), glm::vec3(0.0f), glm::vec2(texCoords[1], texCoords[2])});
     }
 }
 
--- examples/FPS/src/Level.hpp
+++ examples/FPS/src/Level.hpp
@@ -13,7 +13,13 @@ class Level {
         inline const Villain::Material& getMaterial() const { return *material; }
     private:
         void generateLevel(const std::string& tileAtlasFileName);
+
+        // Generate indices for quad
         void addFace(std::vector<unsigned int>* indices, int startLocation, bool direction);
+        // Generate vertices for quad on any plane, used for walls/floors and ceilings
+        void addVertices(std::vector<VertexP1N1UV>* vertices, int i, int j, bool x, bool y, bool z, float offset, float* texCoords);
+
+        // Selects tile from atlas using some value, most likely color component of bitmap
         float* getTexCoords(int val);
 
         Bitmap* bitmap = nullptr;
