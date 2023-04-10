630348e751888cd1655790295e87888ccad22df7
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-04-10 14:35:54 +0100

    FPS: Readd mysteriously deleted door vertices causing UV to get duckedc

--- examples/FPS/src/Door.cpp
+++ examples/FPS/src/Door.cpp
@@ -15,6 +15,8 @@ Door::Door(const glm::vec3& open) : MeshRenderer<VertexP1N1UV>(nullptr, Material
     std::vector<VertexP1N1UV> vertices;
 
     // NOTE: Now Mesh class only needs vertices and indices and it could contain methods to build common primitives like these Also need an easy way to generate UV coords in 3D, and calculate normals vertices.push_back({glm::vec3(Start, Start, Start), glm::vec3(0.0f), glm::vec2(0.5f, 0.f)}); vertices.push_back({glm::vec3(Start, Height, Start), glm::vec3(0.0f), glm::vec2(0.5f, 0.25f)});
+    vertices.push_back({glm::vec3(Start, Start, Start), glm::vec3(0.0f), glm::vec2(0.5f, 0.f)});
+    vertices.push_back({glm::vec3(Start, Height, Start), glm::vec3(0.0f), glm::vec2(0.5f, 0.25f)});
     vertices.push_back({glm::vec3(Length, Height, Start), glm::vec3(0.0f), glm::vec2(0.75f, 0.25f)});
     vertices.push_back({glm::vec3(Length, Start, Start), glm::vec3(0.0f), glm::vec2(0.75f, 0.f)});
 
