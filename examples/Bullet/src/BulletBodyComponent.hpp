3dcb75c76c83f1c8669ee1b3c3bee338c3bc7801
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-04-16 15:47:31 +0100

    cleanup

--- examples/Bullet/src/BulletBodyComponent.hpp
+++ examples/Bullet/src/BulletBodyComponent.hpp
@@ -8,14 +8,15 @@ class BulletBodyComponent: public Villain::NodeComponent {
         BulletBodyComponent(btRigidBody* body): rigidBody(body) {}
 
         virtual void update(float deltaTime) override {
+            // Synchonise node position with bullet rigid body's
             btVector3 origin = rigidBody->getWorldTransform().getOrigin();
             GetTransform()->setPos({origin.getX(), origin.getY(), origin.getZ()});
-
+            // Synchonise node euler rotation with bullet rigid body's
             float x,y,z;
             rigidBody->getWorldTransform().getRotation().getEulerZYX(x, y, z);
-            //printf("Bullet body example: %fX, %fY, %fZ\n", x, y, z);
             GetTransform()->setEulerRot(glm::degrees(x), glm::degrees(y), glm::degrees(z));
         }
+
     private:
         btRigidBody* rigidBody = nullptr;
 };
