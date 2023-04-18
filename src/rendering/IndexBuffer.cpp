3340e3fdb0cd84ed9fddcdb2fa0205d451263b08
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-04-18 09:48:56 +0100

    Engine: added ability to fill IndexBuffer later on, becoming too similar with VertexBuffer class

--- src/rendering/IndexBuffer.cpp
+++ src/rendering/IndexBuffer.cpp
@@ -1,13 +1,12 @@
 #include "IndexBuffer.hpp"
 
-#include "ErrorHandler.hpp"
-
 IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int cnt)
     : count(cnt) {
     GLCall(glGenBuffers(1, &rendererID));
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rendererID));
     // Keep in mind there is a slim chance unsigned int on some platforms will not be 4 bytes, in that case use GLUint
-    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW));
+    if (cnt != -1)
+        GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW));
 }
 
 IndexBuffer::~IndexBuffer() {
@@ -22,3 +21,8 @@ void IndexBuffer::unbind() const {
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 }
 
+void IndexBuffer::fill(const void* data, unsigned int size, GLenum usage) {
+    bind();
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, usage));
+    GLCall(glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, size, data));
+}
--- src/rendering/IndexBuffer.hpp
+++ src/rendering/IndexBuffer.hpp
@@ -1,6 +1,8 @@
 #ifndef __IndexBuffer__
 #define __IndexBuffer__
 
+#include "ErrorHandler.hpp"
+
 class IndexBuffer {
     public:
         IndexBuffer(const unsigned int* data, unsigned int cnt);
@@ -10,6 +12,7 @@ class IndexBuffer {
         void unbind() const;
 
         inline unsigned int getCount() const { return count; }
+        void fill(const void* data, unsigned int size, GLenum usage = GL_STATIC_DRAW);
     private:
         unsigned int rendererID;
         unsigned int count; //<<< Numner of indices
