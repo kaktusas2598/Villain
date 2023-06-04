1f438bfbfbaad86b2aeb983e271035777154ba77
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-06-04 23:50:26 +0100

    clean up

--- res/shaders/cubemap.glsl
+++ res/shaders/cubemap.glsl
@@ -10,7 +10,6 @@ uniform mat4 view;
 
 void main() {
     v_texCoords = position;
-    //gl_Position = projection * view * vec4(position, 1.0);
     vec4 pos = projection * view * vec4(position, 1.0);
     gl_Position = pos.xyww;
 }
