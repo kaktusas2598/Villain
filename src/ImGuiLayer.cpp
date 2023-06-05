5ba2e87bedb001ed1d94c2e7b3a02c536f41db51
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-06-06 00:29:25 +0100

    Using C++'s filesystem to start native file dialog in engine runtime dir

--- src/ImGuiLayer.cpp
+++ src/ImGuiLayer.cpp
@@ -24,6 +24,11 @@
 #include "rendering/MeshUtils.hpp"
 #include "nfd.h"
 
+// TODO: refactor to it's own class
+#include <filesystem>
+namespace fs = std::filesystem;
+
+
 namespace Villain {
 
     bool ImGuiLayer::showDemoWindow = false;
@@ -320,11 +325,18 @@ namespace Villain {
 
             // TEMP TEST CODE
             if (ImGui::Button("Load")) {
+                //std::cout << fs::current_path() << ":\n";
+                //std::string path = fs::current_path();
+                //for (const auto & entry : fs::directory_iterator(path))
+                    //std::cout << entry.path() << std::endl;
+                ////fs::temp_directory_path()
+
+
                 nfdchar_t *outPath;
                 nfdfilteritem_t filterItem[2] = { { "Source code", "c,cpp,cc" }, { "Headers", "h,hpp" } };
                 // TODO: open dialog in project/runtime dir
                 // Possibly write basic imgui file dialog instead using c++ filesystem header and imgui trees
-                nfdresult_t result = NFD_OpenDialog(&outPath, filterItem, 2, NULL);
+                nfdresult_t result = NFD_OpenDialog(&outPath, filterItem, 2, fs::current_path().c_str());
                 if (result == NFD_OKAY) {
                     puts(outPath);
                     NFD_FreePath(outPath);
