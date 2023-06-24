4eb4f0e2569273274be1885bfe0d15d2eed095b9
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-06-24 12:02:39 +0100

    cleanup2

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf tests/out/*
 cmake -S . -B tests/out/ -G"Unix Makefiles" -DVILLAIN_BUILD_TESTS=ON -DVILLAIN_CODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug
-make coverage -C tests/out/
-#./out/VillainTests
+make unit_tests -C tests/out/
+#make coverage -C tests/out/
