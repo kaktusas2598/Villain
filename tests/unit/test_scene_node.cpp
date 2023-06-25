6a8be926369f68a97d9171d12dd8f99b45ca6ae4
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-06-25 14:59:38 +0100

    Notes

--- tests/unit/test_scene_node.cpp
+++ tests/unit/test_scene_node.cpp
@@ -13,11 +13,12 @@ public:
     void handleInput(float deltaTime) override { InputInvoked = true; }
     void update(float deltaTime) override { UpdateInvoked = true; }
     void render(Villain::Shader& shader, Villain::RenderingEngine& renderingEngine, Villain::Camera& camera) override { RenderInvoked = true; }
-    void addToEngine(Villain::Engine* engine) override {}
+    void addToEngine(Villain::Engine* engine) override { AddedToEngine = true; }
 
     bool InputInvoked = false;
     bool UpdateInvoked = false;
     bool RenderInvoked = false;
+    bool AddedToEngine = false;
 };
 
 TEST_CASE("SceneNode Test", "[Scene Graph]") {
@@ -57,6 +58,9 @@ TEST_CASE("SceneNode Test", "[Scene Graph]") {
         REQUIRE(component->RenderInvoked);
         REQUIRE(childComponent->RenderInvoked);
 
+        // TODO: mock Engine
+        //node.setEngine()
+
         REQUIRE(node.findByID(1) == &childNode);
         REQUIRE(node.findByID(100) == nullptr);
         REQUIRE(childNode.getID() == 1);
@@ -69,3 +73,42 @@ TEST_CASE("SceneNode Test", "[Scene Graph]") {
         REQUIRE(node.isSelected());
     }
 }
+
+// TODO: Investigate why memory seems to be cleared in SceneNode destructor, but
+// children and components in this test case still seem to point to some memory after delete is called
+//TEST_CASE("SceneNode Destructor Test", "[Scene Graph]") {
+    //SECTION("Memory Deallocation") {
+        //Villain::SceneNode* rootNode = new Villain::SceneNode("RootNode");
+        //Villain::SceneNode* childNode1 = new Villain::SceneNode("ChildNode1");
+        //Villain::SceneNode* childNode2 = new Villain::SceneNode("ChildNode2");
+
+        //TestComponent* component1 = new TestComponent();
+        //TestComponent* component2 = new TestComponent();
+
+        //rootNode->addComponent(component1);
+        //rootNode->addChild(childNode1);
+        //rootNode->addChild(childNode2);
+
+        //childNode1->addComponent(component2);
+
+        //// Check if the components and child nodes are present before destruction
+        //REQUIRE(rootNode->getComponents().size() == 1);
+        //REQUIRE(rootNode->getChildren().size() == 2);
+        //REQUIRE(childNode1->getComponents().size() == 1);
+
+        //// Printfing memory address of component pointer
+        //printf("%p\n", (void*)&component1);
+        //// Destroy the root node
+        //delete rootNode;
+        //rootNode = nullptr;
+
+
+        //// Check if the memory has been deallocated
+        //// After destruction, accessing the deleted objects will result in undefined behavior, so it's important to check for null pointers
+        //REQUIRE(rootNode == nullptr);
+        //REQUIRE(component1 == nullptr);
+        ////REQUIRE(component2 == nullptr);
+        ////REQUIRE(childNode1 == nullptr);
+        ////REQUIRE(childNode2 == nullptr);
+    //}
+//}
