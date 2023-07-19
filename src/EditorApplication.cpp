2278026c4d83ad94c1e8e92e739011aa1cc58ccc
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-07-20 00:27:10 +0100

    Define cmake target and custom Editor application

---
+++ src/EditorApplication.cpp
@@ -0,0 +1,20 @@
+#include "EditorApplication.hpp"
+
+int main(int argc, char *argv[]) {
+
+    Villain::Editor::EditorApplication editor;
+
+    Villain::Engine engine;
+    engine.init(
+            &editor,
+            "Villain Editor",
+            800,
+            600,
+            0,
+            true
+        );
+
+    engine.run();
+
+    return 0;
+}
