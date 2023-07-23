44b44a4aa7d8c401c7a270aa8d2092a472c8bd62
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-07-23 17:32:39 +0100

    Collision resolution algorithms

--- src/physics/Contact.cpp
+++ src/physics/Contact.cpp
@@ -9,6 +9,92 @@ namespace Villain {
         restituion = restCoef;
     }
 
+    void Contact::calculateInternals(float deltaTime) {
+        // Check if the first object is null and swap it
+        if (!bodies[0]) swapBodies();
+        assert(bodies[0]);
+
+        // Calculate a set of axes at the contact point
+        calculateContactBasis();
+
+        // Store the relative position of the contact relative to each body
+        relativeContactPos[0] = contactPoint - bodies[0]->getPosition();
+        if (bodies[1]) {
+            relativeContactPos[1] = contactPoint - bodies[1]->getPosition();
+        }
+
+        // Find the relative velocity of the bodies at the contact point
+        contactVelocity = calculateLocalVelocity(0, deltaTime);
+        if (bodies[1]) {
+            contactVelocity -= calculateLocalVelocity(1, deltaTime);
+        }
+
+        // Calculate the desired change in velocity for resolution
+        calculateDesiredDeltaVelocity(deltaTime);
+    }
+
+    void Contact::swapBodies() {
+        contactNormal *= -1.0f;
+
+        RigidBody* temp = bodies[0];
+        bodies[0] = bodies[1];
+        bodies[1] = temp;
+    }
+
+    void Contact::matchAwakeState() {
+        // Collision with immovable object won't cause body to wake up
+        if (!bodies[1]) return;
+
+        bool body0Awake = bodies[0]->getAwake();
+        bool body1Awake = bodies[1]->getAwake();
+
+        // Wake up only the sleeping one
+        if (body0Awake ^ body1Awake) {
+            if (body0Awake) bodies[1]->setAwake();
+            else bodies[0]->setAwake();
+        }
+    }
+
+    void Contact::calculateDesiredDeltaVelocity(float deltaTime) {
+        const static float velocityLimit = 0.25f;
+
+        // Calculate the acceleration induced velocity accumulated this frame
+        float velocityFromAcc = 0;
+
+        if (bodies[0]->getAwake()) {
+            velocityFromAcc += glm::dot(bodies[0]->getLastFrameAcceleration() * deltaTime, contactNormal);
+        }
+        if (bodies[1] && bodies[1]->getAwake()) {
+            velocityFromAcc -= glm::dot(bodies[1]->getLastFrameAcceleration() * deltaTime, contactNormal);
+        }
+
+        // If velocity is very slow, limit the restitution
+        float thisRestitution = restituion;
+        if (fabs(contactVelocity.x) < velocityLimit) {
+            thisRestitution = 0.0f;
+        }
+
+        // Combine the bounce velocity with the removed acceleration velocity
+        desiredDeltaVelocity = -contactVelocity.x - thisRestitution * (contactVelocity.x - velocityFromAcc);
+    }
+
+    glm::vec3 Contact::calculateLocalVelocity(unsigned bodyIndex, float deltaTime) {
+        RigidBody* thisBody = bodies[bodyIndex];
+
+        // Work out the velocity of the contact point
+        glm::vec3 velocity = glm::cross(thisBody->getAngularVelocity(), relativeContactPos[bodyIndex]);
+        velocity += thisBody->getLinearVelocity();
+
+        // Turn the velocity into contact coordinates
+        glm::vec3 contactVelocity = glm::transpose(contactToWorld) * velocity;
+
+        // Calculate the amount of velocity that is due to forces without reactions
+        glm::vec3 accVelocity = thisBody->getLastFrameAcceleration() * deltaTime;
+        contactVelocity += accVelocity;
+
+        return contactVelocity;
+    }
+
     void Contact::calculateContactBasis() {
         glm::vec3 contactTangent[2];
 
@@ -42,5 +128,299 @@ namespace Villain {
         }
 
         contactToWorld = glm::mat3(contactNormal, contactTangent[0], contactTangent[1]);
+    }
+
+    void Contact::applyImpulse(const glm::vec3& impulse, RigidBody* body, glm::vec3* linearVelChange, glm::vec3* angularVelChange) {
+        // TODO:
+    }
+
+    void Contact::applyVelocityChange(glm::vec3 linearVelChange[2], glm::vec3 angularVelChange[2]) {
+        // Get inverse mass and inverse inertia tensor in world coords
+        glm::mat3 inverseInertiaTensor[2];
+        inverseInertiaTensor[0] = bodies[0]->getInverseInertiaTensorWorld();
+        if (bodies[1])
+            inverseInertiaTensor[1] = bodies[1]->getInverseInertiaTensorWorld();
+
+        // Will calculate impulse for each contact axis
+        glm::vec3 impulseContact;
+
+        if (friction == 0.0f) {
+            // Calculate short format for frictionless impulse
+            impulseContact = calculateFrictionlessImpulse(inverseInertiaTensor);
+        } else {
+            // TODO:friction impulse
+            assert(false);
+        }
+
+        // Convert impulse to world coords:
+        glm::vec3 impulse = contactToWorld * impulseContact;
+
+        // Split impulse into linear and angular/rotational components
+        glm::vec3 impulsiveTorque = glm::cross(relativeContactPos[0], impulse);
+        angularVelChange[0] = inverseInertiaTensor[0] * impulsiveTorque;
+        linearVelChange[0] = glm::vec3(0.0);
+        linearVelChange[0] += impulse * bodies[0]->getInverseMass();
+
+        // Apply the changes to rigid body
+        bodies[0]->addLinearVelocity(linearVelChange[0]);
+        bodies[0]->addAngularVelocity(angularVelChange[0]);
+
+        if (bodies[1]) {
+            // Split impulse into linear and angular/rotational components
+            impulsiveTorque = glm::cross(relativeContactPos[1], impulse);
+            angularVelChange[1] = inverseInertiaTensor[1] * impulsiveTorque;
+            linearVelChange[1] = glm::vec3(0.0);
+            linearVelChange[1] += impulse * bodies[1]->getInverseMass();
+
+            // Apply the changes to rigid body
+            bodies[1]->addLinearVelocity(linearVelChange[1]);
+            bodies[1]->addAngularVelocity(angularVelChange[1]);
+        }
+    }
+
+    glm::vec3 Contact::calculateFrictionlessImpulse(glm::mat3* inverseInertiaTensor) {
+        glm::vec3 impulseContact;
+
+        // Calculate vector that shows the change of velicity in world space for a unit impulse
+        // in the direction of the contact normal
+        glm::vec3 deltaVelWorld = glm::cross(relativeContactPos[0], contactNormal);
+        deltaVelWorld = inverseInertiaTensor[0] * deltaVelWorld;
+        deltaVelWorld = glm::cross(deltaVelWorld, contactNormal);
+
+        // Find change in velocity in contact coords
+        float deltaVelocity = glm::dot(deltaVelWorld, contactNormal);
+
+        // Add linear component to change
+        deltaVelocity += bodies[0]->getInverseMass();
+
+        if (bodies[1]) {
+            // Same transform sequence again
+            deltaVelWorld = glm::cross(relativeContactPos[1], contactNormal);
+            deltaVelWorld = inverseInertiaTensor[1] * deltaVelWorld;
+            deltaVelWorld = glm::cross(deltaVelWorld, contactNormal);
+
+            deltaVelocity = glm::dot(deltaVelWorld, contactNormal);
+            deltaVelocity += bodies[1]->getInverseMass();
+        }
+
+        // Calculate the required size of the impulse
+        impulseContact.x = desiredDeltaVelocity / deltaVelocity;
+        impulseContact.y = 0;
+        impulseContact.z = 0;
+        return impulseContact;
+    }
+
+    void Contact::applyPositionChange(glm::vec3 linearChange[2], glm::vec3 angularChange[2], float penetration) {
+        const float angularLimit = 0.2f;
+        float angularMove[2];
+        float linearMove[2];
+
+        float totalInertia = 0;
+        float linearInertia[2];
+        float angularInertia[2];
+
+        // Work out the inertia of each object in the direction of the contact normal, due to angular inertia only
+        for (unsigned i = 0; i < 2; i++) {
+            if (bodies[i]) {
+                glm::mat3 inverseInertiaTensor = bodies[i]->getInverseInertiaTensorWorld();
+
+                glm::vec3 angularInertiaWorld = glm::cross(relativeContactPos[i], contactNormal);
+                angularInertiaWorld = inverseInertiaTensor * angularInertiaWorld;
+                angularInertia[i] = glm::dot(angularInertiaWorld, contactNormal);
+
+                // Linear component is just inverse mass
+                linearInertia[i] = bodies[i]->getInverseMass();
+
+                // Keep track of total inertia forom all components;
+                totalInertia += linearInertia[i] + angularInertia[i];
+
+                // Break the loop here so that totalInertia value is completely calculated
+                // by both iterations before continuing
+            }
+        }
+
+        // Loop again calculating and applying the changes
+        for (unsigned i = 0; i < 2; i++) {
+            if (bodies[i]) {
+                // Linear and angular movements are proportional to the two inverse inertias
+                float sign = (i == 0) ? 1 : -1;
+                angularMove[i] = sign * penetration * (angularInertia[i] / totalInertia);
+                linearMove[i] = sign * penetration * (linearInertia[i] / totalInertia);
+
+                // To avoid angular projections that are too great (when mass is large but inertia tensor is small)
+                // limit the angular move
+                glm::vec3 projection = relativeContactPos[i];
+                projection += contactNormal * (-glm::dot(relativeContactPos[i], contactNormal));
+
+                // Use the small angle approximation for the sine of the angle
+                float maxMagnitude = angularLimit * glm::length(projection);
+
+                if (angularMove[i] < -maxMagnitude) {
+                    float totalMove = angularMove[i] + linearMove[i];
+                    angularMove[i] = -maxMagnitude;
+                    linearMove[i] = totalMove - angularMove[i];
+                } else if (angularMove[i] > maxMagnitude) {
+                    float totalMove = angularMove[i] + linearMove[i];
+                    angularMove[i] = maxMagnitude;
+                    linearMove[i] = totalMove - angularMove[i];
+                }
+
+                // Now we have a linear amount of movement required by turning rigid body(angularMove[i])
+                // So need to calculate the desired rotation to achieve that
+                if (angularMove[i] == 0) {
+                    // No angular movement - so no rotation
+                    angularChange[i] = glm::vec3(0.0);
+                } else {
+                    // Work out the direction to rotate in
+                    glm::vec3 targetAngularDir = glm::cross(relativeContactPos[i], contactNormal);
+
+                    glm::mat3 inverseInertiaTensor = bodies[i]->getInverseInertiaTensorWorld();
+
+                    // Work out the direction we'd need to rotate to achieve that
+                    angularChange[i] = inverseInertiaTensor * targetAngularDir * (angularMove[i] / angularInertia[i]);
+                }
+
+                // Linear velocity change is easier - it's just inear movement along the contact normal
+                linearChange[i] = contactNormal * linearMove[i];
+
+                // Now we can apply values we calculated to the rigid body
+                // Apply the linear movement
+                glm::vec3 pos = bodies[i]->getPosition();
+                pos += linearChange[i];
+                bodies[i]->setPosition(pos);
+
+                // Apply the orientation
+                glm::quat q = bodies[i]->getOrientation();
+                // NOTE: is this correct?
+                q += glm::quat(angularChange[i]);
+                bodies[i]->setOrientation(q);
+
+                // Finally need to calculate derived data for any body that is asleep, to reflect
+                // the changes in the object's data, otherwise resolution won't change object's position
+                // and the next collision detection will have the same penetration
+                if (!bodies[i]->getAwake()) bodies[i]->calculateDerivedData();
+            }
+        }
+    }
+
+    void ContactResolver::resolveContacts(Contact* contactArray, unsigned numContacts, float deltaTime) {
+        // Make sure there are contacts
+        if (numContacts == 0) return;
+
+        // Prepare the contacts for processing
+        prepareContacts(contactArray, numContacts, deltaTime);
+
+        // Resolve the interpenetration problems with the contacts
+        adjustPositions(contactArray, numContacts, deltaTime);
+
+        // Resolve the velocoty problems with the contacts
+        adjustVelocities(contactArray, numContacts, deltaTime);
+    }
+
+    void ContactResolver::prepareContacts(Contact* contactArray, unsigned numContacts, float deltaTime) {
+        // Generate contact velocity and axis information
+        Contact* lastContact = contactArray + numContacts;
+        for (Contact* contact = contactArray; contact < lastContact; contact++) {
+            // Calculate internal contact data (inertia, basis, etc.)
+            contact->calculateInternals(deltaTime);
+        }
+    }
+
+    void ContactResolver::adjustVelocities(Contact* contactArray, unsigned numContacts, float deltaTime) {
+        glm::vec3 linearVelChange[2], angularVelChange[2];
+        glm::vec3 deltaVel;
+
+        // Iteratively resolv interpenetrations in order of severity
+        velocityIterationsUsed = 0;
+        while (velocityIterationsUsed < velocityIterations) {
+            // Find contact with maximum magnitude  probable velocity change
+            float max = velocityEpsilon;
+            unsigned index = numContacts;
+            for (unsigned i = 0; i < numContacts; i++) {
+                if (contactArray[i].desiredDeltaVelocity > max) {
+                    max = contactArray[i].desiredDeltaVelocity;
+                    index = i;
+                }
+            }
+            if (index == numContacts) break;
+
+            // Match the awake state at the contact
+            contactArray[index].matchAwakeState();
+
+            // Resolve velocity
+            contactArray[index].applyVelocityChange(linearVelChange, angularVelChange);
+
+            // After changing velocity of two bodies causes some of the relative closing velocities recomputation
+            for (unsigned i = 0; i < numContacts; i++) {
+                // Check each body in the contact
+                for (unsigned b = 0; b < 2; b++) {
+                    if (contactArray[i].bodies[b]) {
+                        // Check for a match with each body in the newly resolved contact
+                        for (unsigned d = 0; d < 2; d++) {
+                            if (contactArray[i].bodies[b] == contactArray[index].bodies[d]) {
+                                deltaVel = linearVelChange[d] + glm::cross(angularVelChange[d], contactArray[i].relativeContactPos[b]);
+
+                                // The sign of change is positive if dealing with second body in a contact, and
+                                // negative otherwise
+                                contactArray[i].contactVelocity +=
+                                    glm::transpose(contactArray[i].contactToWorld) * deltaVel * (b ? 1.0f: -1.0f);
+                                contactArray[i].calculateDesiredDeltaVelocity(deltaTime);
+                            }
+                        }
+                    }
+                }
+            }
+            velocityIterationsUsed++;
+        }
+    }
+
+    void ContactResolver::adjustPositions(Contact* contactArray, unsigned numContacts, float deltaTime) {
+        unsigned i, index;
+        glm::vec3 linearChange[2], angularChange[2];
+        float max;
+        glm::vec3 deltaPosition;
+
+        // Iteratively resolv interpenetrations in order of severity
+        positionIterationsUsed = 0;
+        while (positionIterationsUsed < positionIterations) {
+            // Find biggest penetration
+            max = positionEpsilon;
+            index = numContacts;
+            for (i = 0; i < numContacts; i++) {
+                if (contactArray[i].penetration > max) {
+                    max = contactArray[i].penetration;
+                    index = i;
+                }
+            }
+            if (index == numContacts) break;
+
+            // Match the awake state at the contact
+            contactArray[index].matchAwakeState();
+
+            // Resolve the penetration
+            contactArray[index].applyPositionChange(linearChange, angularChange, max);
+
+            // This action might have changed the penetration of other bodies so need to update contacts
+            for (i = 0; i < numContacts; i++) {
+                // Check each body in the contact
+                for (unsigned b = 0; b < 2; b++) {
+                    if (contactArray[i].bodies[b]) {
+                        // Check for a match with each body in the newly resolved contact
+                        for (unsigned d = 0; d < 2; d++) {
+                            if (contactArray[i].bodies[b] == contactArray[index].bodies[d]) {
+                                deltaPosition = linearChange[d]
+                                    + glm::cross(angularChange[d], contactArray[i].relativeContactPos[b]);
+
+                                // The sign of change is positive if dealing with second body in a contact, and
+                                // negative otherwise
+                                contactArray[i].penetration +=
+                                    glm::dot(deltaPosition, contactArray[i].contactNormal) * (b ? 1: -1);
+                            }
+                        }
+                    }
+                }
+            }
+            positionIterationsUsed++;
+        }
     }
 }
--- src/physics/Contact.hpp
+++ src/physics/Contact.hpp
@@ -4,6 +4,8 @@
 
 namespace Villain {
 
+    class ContactResolver;
+
     /*! \brief Represent contact of 2 colliding bodies.
      *
      * Only represent contact data between 2 colliding rigid bodies, use ContactResolver
@@ -11,6 +13,8 @@ namespace Villain {
      */
     class Contact {
         public:
+            friend class ContactResolver;
+
             RigidBody* bodies[2]; ///< Bodies involved in the contact. 2nd body can be nullptr for contacts with scenery.
             float friction; ///< Lateral friction coefficient at the contact
             float restituion; ///< Normal restitution coefficient at the contact to describe response bounciness
@@ -22,17 +26,73 @@ namespace Villain {
             /// Set properties that don't normally depend on collision point (bodies and material properties)
             void setBodyData(RigidBody* one, RigidBody* two, float frictionCoef, float restCoef);
 
+        protected:
+            /// Should be called automatically before resolution algorithm is run
+            void calculateInternals(float deltaTime);
+            /// Reverses the contact, calculateInternals() must be called after doing this!
+            void swapBodies();
+            /// Update awake state of bodies that are in contact
+            void matchAwakeState();
+            /// Calculate internal value for the desired delta velocity
+            void calculateDesiredDeltaVelocity(float deltaTime);
+            /// Caclulates the velocity of the contact point at the given body
+            glm::vec3 calculateLocalVelocity(unsigned bodyIndex, float deltaTime);
             /// Calculates an orthonormal basis 3x3 matrix for the contact point, based on the primary
             /// friction direction (for anisotropic friction) or a random direction(for isotropic friction)
             void calculateContactBasis();
 
-        protected:
+            /// Applies impulse to the given body return changes in velocities
+            void applyImpulse(const glm::vec3& impulse, RigidBody* body, glm::vec3* linearVelChange, glm::vec3* angularVelChange);
+            /// Perform inertia-weighted impule based resolution of this contact alone
+            void applyVelocityChange(glm::vec3 linearVelChange[2], glm::vec3 angularVelChange[2]);
+            /// Perform inertia-weighted penetration resolution of this contact alone
+            void applyPositionChange(glm::vec3 linearChange[2], glm::vec3 angularChange[2], float penetration);
+
+            /// Calculate the impulse needed to resolve this contact given that the contact has no friction
+            glm::vec3 calculateFrictionlessImpulse(glm::mat3* inverseInertiaTensor);
+
             /// Converts local contact space coordinates to world space. Columns of this matrix form
             /// an orthonormal set of vectors
             glm::mat3 contactToWorld;
+
+            glm::vec3 contactVelocity; ///< Closing velocity at the point of contact
+            float desiredDeltaVelocity; ///< Required change in velocity to resolve this contact
+            glm::vec3 relativeContactPos[2]; ///< World-space position of contact point relative to center of each body
         private:
     };
 
-    // TODO:
-    class ContactResolver {};
+    /*! \brief Contact resolution routine
+     *
+     * Contact resolver instance can be shared for the entire physics simulation.
+     */
+    class ContactResolver {
+        public:
+            ContactResolver(unsigned iterations, float velEpsilon = 0.01f, float posEpsilon = 0.01f)
+                : velocityIterations(iterations), positionIterations(iterations),
+                velocityEpsilon(velEpsilon), positionEpsilon(posEpsilon) {}
+
+            /// Resolves set of contacts for both velocity and penetration. Contacts that cannot intersect
+            /// with each other, should be pased to separate calls of resolveContacts, better to do many calls
+            /// with smaller sets.
+            void resolveContacts(Contact* contactArray, unsigned numContacts, float deltaTime);
+
+            unsigned velocityIterations; ///< Number of iterations to performe when resolving velocity
+            unsigned positionIterations; ///< Number of iterations to performe when resolving position
+
+            float velocityEpsilon = 0.01f; ///< To reduce instability
+            float positionEpsilon = 0.01f; ///< To reduce instability
+
+            unsigned velocityIterationsUsed; ///< Number of velocity iterations used in the last call to resolve contacts
+            unsigned positionIterationsUsed; ///< Number of position iterations used in the last call to resolve contacts
+
+        protected:
+            /// Calculate internal data for contacts and make sure correct bodies are alive
+            void prepareContacts(Contact* contactArray, unsigned numContacts, float deltaTime);
+
+            /// Resolves velocity issues for a given array of constraints, using given number of iterations
+            void adjustVelocities(Contact* contactArray, unsigned numContacts, float deltaTime);
+
+            /// Resolves positional issues for a given array of constraints, using given number of iterations
+            void adjustPositions(Contact* contactArray, unsigned numContacts, float deltaTime);
+    };
 }
