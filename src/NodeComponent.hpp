a89a4d815a5143412fed567ddb54ccb08f2cf4c2
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-07-27 17:55:08 +0100

    Commenting NodeComponent, adding macro to auto generate unique ids

--- src/NodeComponent.hpp
+++ src/NodeComponent.hpp
@@ -2,12 +2,16 @@
 
 #include "SceneNode.hpp"
 
+
+/// Generates unique id per component class, usually called in constructor
+#define VILLAIN_SET_COMPONENT_ID(ComponentClass) \
+    id = GetId<ComponentClass>()
+
 namespace Villain {
 
-    // Component signature idea borrowed from ECS designs
-    // Generate unique ID for every new component, eg:
-    // int id = GetId<CameraComponent>();
-    //
+    /// Component signature idea borrowed from ECS designs
+    /// Generate unique ID for every new component, eg:
+    /// int id = GetId<CameraComponent>();
     extern int componentCounter;
     template <class T> int GetId() {
         static int componentId = componentCounter++;
@@ -17,30 +21,40 @@ namespace Villain {
     class RenderingEngine;
     class Shader;
 
-    // Defines logic for Scene Node
-    // Kind of like ECS components and systems combined, not ideal approach but wayyy beter
-    // than inheritance approach
+    /*! \brief Component for Scene Graph node
+     *
+     * Lets define custom data and logic and attach it to any scene graph node. Each scene graph node contains
+     * a collection of node components, basically Composition approach, but could evolve to ECS in the future
+     */
     class NodeComponent {
         public:
-            NodeComponent() :
-                parent(0) {}
+            NodeComponent() : parent(nullptr) {}
             virtual ~NodeComponent() {}
 
+            /// Each component can optionally implement logic to handle events
             virtual void handleInput(float deltaTime) {}
+            /// Each component can optionally implement update logic
             virtual void update(float deltaTime) {}
+            /// Each component can optionally implement render logic
             virtual void render(Shader& shader, RenderingEngine& renderingEngine, Camera& camera) {}
 
+            // Will be called after node containing this component gets added to scene graph or if we
+            // add this component to node after that node was already added to scene graph
             virtual void addToEngine(Engine* engine) { }
 
+            /// Transform will be calculated by traversing scene graph hierarchy up to root node
             inline Transform* GetTransform()             { return parent->getTransform(); }
             inline const Transform& GetTransform() const { return *parent->getTransform(); }
 
+            /// Get identifier, unique to each type of component
             int getID() const { return id; }
+            /// Get scene graph node which owns this component
             inline SceneNode* getParent() { return parent; }
+            /// Set owner scene graph node of this parent, called automatically
             virtual void setParent(SceneNode* p) { parent = p; }
         protected:
-            SceneNode* parent;
-            int id = -1;
+            SceneNode* parent; ///< Scene graph node which has ownership of this component
+            int id = -1; ///< Identifier, unique to each type of component
         private:
             NodeComponent(const NodeComponent& other) {}
             void operator=(const NodeComponent& other) {}
