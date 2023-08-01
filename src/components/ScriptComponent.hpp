1f452b735a0786468b56cc046bcbaa51396e01d1
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2023-08-01 12:15:43 +0100

    Splitting up ScriptComponent into header and source

--- src/components/ScriptComponent.hpp
+++ src/components/ScriptComponent.hpp
@@ -6,21 +6,14 @@
 
 namespace Villain {
 
+    /// Allows attaching custom Lua logic to a node in scene graph
     class ScriptComponent : public NodeComponent {
         public:
-            ScriptComponent(const std::string& scriptFilename): script(scriptFilename) {
-                LuaBindings::registerBindings(script.getLuaState());
-            }
+            ScriptComponent(const std::string& scriptFilename);
             ~ScriptComponent() {}
 
-            virtual void update(float deltaTime) override {
-                // TODO: Instead calling scritp, call specific function from the script like 'update' ?
-                //To call a Lua function named "update" defined in the Lua script:
-                // lua_getglobal(luaScript.getLuaState(), "update");
-                // lua_pushnumber(luaScript.getLuaState(), deltaTime);
-                // lua_pcall(luaScript.getLuaState(), 1, 0, 0);
-                script.open();
-            }
+            /// Calls Lua "update" function
+            virtual void update(float deltaTime) override;
 
         private:
             LuaScript script;
