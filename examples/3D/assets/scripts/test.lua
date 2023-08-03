c304934148213b333a3f99201edc45daba125802
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-08-03 19:17:09 +0100

    Update lua script

--- examples/3D/assets/scripts/test.lua
+++ examples/3D/assets/scripts/test.lua
@@ -1,14 +1,23 @@
-function update(deltaTime, node)
-    if (deltaTime > 0.02) then
-        print("Delta time: " .. deltaTime)
+function Update(deltaTime, node)
+    if (deltaTime > 0.017) then
+        print("LOW FPS! Delta time: " .. deltaTime)
     end
     --print("Screen width: " .. getScreenWidth())
     --print("Node name: " .. node:getName())
 end
 
 -- Optional callback
-function onKey(key, node)
+function OnKey(key, node)
     print("Key: " .. string.char(key))
     print("Node name: " .. node:getName())
+
+    if (node:getComponent("Camera")) then
+        print("Camera compo found!")
+    end
+
+    if (key == 114) then -- 114 is code for 'r' in SDL2/Villain::KeyCode
+        print("Setting node pos")
+        node:setPosition(0, 10, 0)
+    end
 end
 
