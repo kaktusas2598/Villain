e521b790bc869e53779f4db97a51bc8cd9b1373f
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-08-07 00:36:24 +0100

    Improve configure script, add getopts

--- examples/3D/configure.sh
+++ examples/3D/configure.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-#git submodule add https://github.com/g-truc/glm.git vendor/glm
 git submodule update --init --recursive
-
 rm -rf out/*
-# Can specify option with cmake -DOPTION_NAME=VALUE
-#cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo
-#cmake -DCMAKE_BUILD_TYPE=Debug
-cmake -S . -B out/ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo
-#-G"Visual Studio 15 2017"
+
+
+OPTIND=1 # Reset for previous uses of getops in shell
+OPTSTRING="rdb"
+BUILD_TYPE="RelWithDebInfo"
+BUILD=OFF
+
+# Parse arguments
+while getopts "$OPTSTRING" opt; do
+    case "$opt" in
+    r)
+        BUILD_TYPE="Release"
+        ;;
+    d)
+        BUILD_TYPE="Debug"
+        ;;
+    b)
+        BUILD=ON
+        ;;
+    *)
+        echo "Invalid option: $1" >&2
+        exit 1
+        ;;
+    esac
+done
+
+cmake -S . -B out/ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"
+#cmake -S . -B out/ -G"Visual Studio 15 2017" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"
+
+if [ "$BUILD" = "ON" ]; then
+    make -C out/
+fi
