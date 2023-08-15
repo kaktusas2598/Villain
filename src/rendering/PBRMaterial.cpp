0d66ff78e868542e820e40d86ca4b5561ebaf3c2
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-08-15 01:46:14 +0100

    Improve uniform name

--- src/rendering/PBRMaterial.cpp
+++ src/rendering/PBRMaterial.cpp
@@ -10,46 +10,46 @@ namespace Villain {
 
         // Base color map
         if (albedoMap == nullptr) {
-            shader.setUniform1i("material.useAlbedoMap", 0);
+            shader.setUniform1i("pbrMaterial.useAlbedoMap", 0);
         } else {
-            shader.setUniform1i("material.useAlbedoMap", 1);
+            shader.setUniform1i("pbrMaterial.useAlbedoMap", 1);
             albedoMap->bind(RenderingEngine::getSamplerSlot("albedo"));
-            shader.setUniform1i("material.texture_albedo", RenderingEngine::getSamplerSlot("albedo"));
+            shader.setUniform1i("pbrMaterial.texture_albedo", RenderingEngine::getSamplerSlot("albedo"));
         }
         // Normal/bump map
         if (normalMap == nullptr) {
-            shader.setUniform1i("material.useNormalMap", 0);
+            shader.setUniform1i("pbrMaterial.useNormalMap", 0);
         } else {
-            shader.setUniform1i("material.useNormalMap", 1);
+            shader.setUniform1i("pbrMaterial.useNormalMap", 1);
             normalMap->bind(RenderingEngine::getSamplerSlot("normal"));
-            shader.setUniform1i("material.texture_normal", RenderingEngine::getSamplerSlot("normal"));
+            shader.setUniform1i("pbrMaterial.texture_normal", RenderingEngine::getSamplerSlot("normal"));
         }
 
         // Metallic map
         if (metallicMap == nullptr) {
-            shader.setUniform1i("material.useMetallicMap", 0);
+            shader.setUniform1i("pbrMaterial.useMetallicMap", 0);
         } else {
-            shader.setUniform1i("material.useMetallicMap", 1);
+            shader.setUniform1i("pbrMaterial.useMetallicMap", 1);
             metallicMap->bind(RenderingEngine::getSamplerSlot("metallic"));
-            shader.setUniform1i("material.texture_metallic", RenderingEngine::getSamplerSlot("metallic"));
+            shader.setUniform1i("pbrMaterial.texture_metallic", RenderingEngine::getSamplerSlot("metallic"));
         }
 
         // Roughness map
         if (roughnessMap == nullptr) {
-            shader.setUniform1i("material.useRoughnessMap", 0);
+            shader.setUniform1i("pbrMaterial.useRoughnessMap", 0);
         } else {
-            shader.setUniform1i("material.useRoughnessMap", 1);
+            shader.setUniform1i("pbrMaterial.useRoughnessMap", 1);
             roughnessMap->bind(RenderingEngine::getSamplerSlot("roughness"));
-            shader.setUniform1i("material.texture_roughness", RenderingEngine::getSamplerSlot("roughness"));
+            shader.setUniform1i("pbrMaterial.texture_roughness", RenderingEngine::getSamplerSlot("roughness"));
         }
 
         // Ambient oclussion map
         if (aoMap == nullptr) {
-            shader.setUniform1i("material.useAOMap", 0);
+            shader.setUniform1i("pbrMaterial.useAOMap", 0);
         } else {
-            shader.setUniform1i("material.useAOMap", 1);
+            shader.setUniform1i("pbrMaterial.useAOMap", 1);
             aoMap->bind(RenderingEngine::getSamplerSlot("ao"));
-            shader.setUniform1i("material.texture_ao", RenderingEngine::getSamplerSlot("ao"));
+            shader.setUniform1i("pbrMaterial.texture_ao", RenderingEngine::getSamplerSlot("ao"));
         }
 
         // TODO: Parallax displacement map
