f7eb87df4b5a14b81eedd29b396d7a17f8005f22
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-08-22 23:32:38 +0100

    New shaders to convert HDR equirectangular texture to cubemap texture and one to render env map, super similar to skybox shader

---
+++ res/shaders/envmap.glsl
@@ -0,0 +1,36 @@
+#shader vertex
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 projection;
+uniform mat4 view;
+
+out vec3 localPos;
+
+void main()
+{
+    localPos = aPos;
+
+    mat4 rotView = mat4(mat3(view)); // remove translation from the view matrix
+    vec4 clipPos = projection * rotView * vec4(localPos, 1.0);
+
+    gl_Position = clipPos.xyww;
+}
+
+#shader fragment
+#version 330 core
+out vec4 FragColor;
+
+in vec3 localPos;
+
+uniform samplerCube environmentMap;
+
+void main()
+{
+    vec3 envColor = texture(environmentMap, localPos).rgb;
+
+    envColor = envColor / (envColor + vec3(1.0));
+    envColor = pow(envColor, vec3(1.0/2.2));
+
+    FragColor = vec4(envColor, 1.0);
+}
